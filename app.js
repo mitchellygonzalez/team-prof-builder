2fb74a81050fa5dd0a1162145ce5826a8bb1fb28
mitchellygonzalez <107009737+mitchellygonzalez@users.noreply.github.com>
2022-11-15 21:45:21 -0500

    save changes before leaving

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
 const inquierer = require ('inquirer');
+const fs = require("fs");
+const path = require ("path");
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+
 const Manager = require (".lib/Manager");
 const Engineer = require (".lib/Engineer");
 const Intern = require (".lib/Intern");
 
-const fs = require("fs");
-const path = require ("path");
+
+const createManager =  = () => {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is your name?",
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log("Please enter your name");
+                    return false;
+                }
+            }
+        },
